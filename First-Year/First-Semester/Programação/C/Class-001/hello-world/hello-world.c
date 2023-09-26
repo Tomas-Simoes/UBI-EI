97b6e05b40a75bd19f5ceb1d7174cdf7885b5f21
Tomas-Simoes <tomas20simoes@gmail.com>
2023-09-26 14:11:59 +0100

    First and second class of LP and first of P

---
+++ First-Year/First-Semester/Programação/C/Class-001/hello-world/hello-world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+  int age = 0;
+  printf("hello world :') (again)");
+  scanf("%d", &age);
+}
---
+++ First-Year/First-Semester/Programação/C/Class-001/scanf/scanf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+  int age;
+  
+  printf("What's your age? \n");
+  scanf("%d", &age);
+  printf("\nYou are %d years old.", age);
+}
