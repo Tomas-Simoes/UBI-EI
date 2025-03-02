a29f66ecfeccba5de3f85a6a8388fcad7d40b10a
Tomas-Simoes <tomas20simoes@gmail.com>
2025-03-02 18:53:33 +0000

    SO class 01 finished

--- s_year/s_semester/so/practical_classes/class01/class01.c
+++ s_year/s_semester/so/practical_classes/class01/class01.c
@@ -15,21 +15,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-void streamCopy(FILE* in, FILE* out);
+void streamCopy(FILE* in, FILE* out, bool showControlCh, bool printNumberLine);
 
 int main(int argc, char* argv[]){
+    bool showControlCh = false;
+    bool printNumberLine = false;
+    int startIndex = 1;
+    
     if(argc == 1) {
-        streamCopy(stdin, stdout);
+        streamCopy(stdin, stdout, false, false);
     } else {
         for (int i = 1; i < argc; i++)
+        {
+            if(argv[i][0] != '-') {
+                startIndex = i;
+                break;
+            }
+            else {
+                if (strcmp(argv[i], "-T") == 0) showControlCh = true;
+                else if (strcmp(argv[i], "-n") == 0) printNumberLine = true;
+                else fprintf(stderr, "Fatal error: Invalid parameter %s.", argv[i]);
+            }
+        }
+        
+        for (int i = startIndex; i < argc; i++)
         {
             FILE* file = fopen(argv[i], "r");
 
             if (file == NULL) {
-                fprintf(stderr, "%s: Fatal error", argv[i]);
+                fprintf(stderr, "Fatal error: File %s not found.", argv[i]);
+                return 0;
             } else {
-                streamCopy(file, stdout);
+                streamCopy(file, stdout, showControlCh, printNumberLine);
                 fclose(file);
             }
         }
@@ -39,10 +59,20 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-void streamCopy(FILE* in, FILE* out) {
+
+
+void streamCopy(FILE* in, FILE* out, bool showControlCh, bool printNumberLine) {
     int ch;
+    int lineCount = 1;
+    
+    fprintf(out, "%d ", lineCount);
     while ((ch=fgetc(in)) != EOF)
     {
-        fputc(ch, out);
+        if(ch == '\t' && showControlCh) fputs("TAB", out);
+        else fputc(ch, out);
+
+        if(ch == '\n'){
+            fprintf(out, "%d ", lineCount++);
+        }
     }
 }
