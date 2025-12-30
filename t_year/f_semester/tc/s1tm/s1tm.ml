c03ba99c34e29b92ffe2d686da87f401f374fd2c
Tomas-Simoes <tomas20simoes@gmail.com>
2025-12-30 18:04:35 +0000

    tc test

--- t_year/f_semester/tc/s1tm/s1tm.ml
+++ t_year/f_semester/tc/s1tm/s1tm.ml
@@ -12,7 +12,6 @@ let simbolo_of_yaml = function
 
 type direction = L | R | S
 
-(* Custom serialization/deserialization for direction *)
 let direction_to_yaml = function
   | L -> `String "L"
   | R -> `String "R"
@@ -28,7 +27,6 @@ type transition = estado * simbolo * direction [@@deriving yaml]
 
 type delta = (string, (string, transition) Hashtbl.t) Hashtbl.t
 
-(* Custom serialization/deserialization for delta *)
 let delta_to_yaml delta =
   `O
     (Hashtbl.fold
@@ -84,16 +82,14 @@ let rec read_multiplelines () =
   with End_of_file -> ""
 
 let yaml_val = function
-  | Ok v -> v (* v has type Yaml.value *)
+  | Ok v -> v 
   | Error (`Msg m) -> failwith m
 
-(* string para tm_w *)
 let tm_w_de_string s = s |> Yaml.of_string_exn |> tm_w_of_yaml |> yaml_val
 
-(* tm_w para string *)
 let tm_w_para_string tm_w = tm_w |> tm_w_to_yaml |> Yaml.to_string_exn
 
-let verbose = false (* Toggle to false to hide debug steps *)
+let verbose = true 
 
 let enable f = if verbose then f () else ()
 
@@ -108,11 +104,32 @@ let update_tape tape index new_symbol =
     Bytes.set tape' index new_symbol;
     Bytes.to_string tape'
 
-let rec check_tape tm tape t_index cur_state =
-  if t_index < 0 then (
+let validate_input tm w =
+  let input_set = List.fold_left (fun acc s -> acc || s = "") false tm.input_alphabet in
+  if input_set then true 
+  else
+    let rec check_chars i =
+      if i >= String.length w then true
+      else
+        let c = String.make 1 w.[i] in
+        if List.mem c tm.input_alphabet then check_chars (i + 1)
+        else false
+    in
+    String.length w = 0 || check_chars 0
+
+let rec check_tape tm tape t_index cur_state steps max_steps =
+  if cur_state = tm.accept_state then Some (true, tape)
+  else if cur_state = tm.reject_state then Some (false, tape)
+  
+  else if steps >= max_steps then (
+    debug_print "Reached maximum steps (%d) - DON'T KNOW" max_steps;
+    None
+  )
+  else if t_index < 0 then (
     debug_print "Head moved left of index 0 - REJECT";
     Some (false, tape)
-  ) else
+  ) 
+  else
     let cur_char = if t_index >= String.length tape then '_' else tape.[t_index] in
     let cur_char_str = String.make 1 cur_char in
     
@@ -127,14 +144,19 @@ let rec check_tape tm tape t_index cur_state =
             Some (false, tape)
         | Some (new_state, new_symbol, direction) ->
             let new_tape = update_tape tape t_index new_symbol.[0] in 
-            let new_t_index = match direction with L -> t_index - 1 | R -> t_index + 1 | S -> t_index in 
+            
+            let new_t_index = match direction with 
+              | L -> if t_index = 0 then 0 else t_index - 1 
+              | R -> t_index + 1 
+              | S -> t_index 
+            in 
             
             debug_print "State: %s | Read: %s | Write: %s | Move: %s | New Tape: %s" 
                         cur_state cur_char_str new_symbol (match direction with L->"L"|R->"R"|S->"S") new_tape;
 
             if new_state = tm.accept_state then Some (true, new_tape) 
             else if new_state = tm.reject_state then Some (false, new_tape)
-            else check_tape tm new_tape new_t_index new_state
+            else check_tape tm new_tape new_t_index new_state (steps + 1) max_steps
 
 let () =  
   let _ = read_multiplelines in 
@@ -145,11 +167,24 @@ let () =
   let tm_w_obj = match s |> Yaml.of_string_exn |> tm_w_of_yaml with
     | Ok v -> v | Error (`Msg m) -> failwith m in
   
-  match check_tape tm_w_obj.m tm_w_obj.w 0 tm_w_obj.m.start_state with
-  | Some (true, final_tape) ->
-      print_endline "YES";
-      Printf.printf "%s_\n" final_tape
-  | Some (false, final_tape) ->
-      print_endline "NO";
-      Printf.printf "%s_\n" final_tape
-  | None -> print_endline "DON'T KNOW"
+  if not (validate_input tm_w_obj.m tm_w_obj.w) then
+    print_endline "INVALID"
+  else
+    match check_tape tm_w_obj.m tm_w_obj.w 0 tm_w_obj.m.start_state 0 200 with
+    | Some (true, final_tape) ->
+        print_endline "YES";
+        let output_tape = 
+          if String.length final_tape > 0 && final_tape.[String.length final_tape - 1] = '_' 
+          then final_tape 
+          else final_tape ^ "_" 
+        in
+        Printf.printf "%s\n" output_tape
+    | Some (false, final_tape) ->
+        print_endline "NO";
+        let output_tape = 
+          if String.length final_tape > 0 && final_tape.[String.length final_tape - 1] = '_' 
+          then final_tape 
+          else final_tape ^ "_" 
+        in
+        Printf.printf "%s\n" output_tape
+    | None -> print_endline "DON'T KNOW"
